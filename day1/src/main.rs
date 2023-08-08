4e5928c3f04218665125edeb389149a290f764fe
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2023-08-08 18:55:43 +0300

    Solve part2 of day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,20 @@
 use itertools::Itertools;
+use std::cmp::Reverse;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
+    let answer = include_str!("input.txt")
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .batching(|it| it.map_while(|x| x).sum1::<u64>())
+        .map(Reverse)
+        .k_smallest(3)
+        .map(|x| x.0)
+        .sum::<u64>();
+    println!("{answer:?}");
+
+    /* part 1 ---
     let max = include_str!("input.txt")
         .lines()
         .map(|v| v.parse::<u64>().ok())
@@ -15,6 +27,6 @@ fn main() -> color_eyre::Result<()> {
         })
         .max();
     println!("{max:?}");
-
+    */
     Ok(())
 }
