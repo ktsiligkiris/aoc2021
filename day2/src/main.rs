05575aba67d99c391794ab21c69ebb54fd6af84e
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2023-08-08 21:48:14 +0300

    Solve day 2 part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,116 @@
+use color_eyre::Report;
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Round {
+    theirs: Move,
+    ours: Move,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Outcome {
+    Win,
+    Draw,
+    Loss,
+}
+
+impl TryFrom<char> for Move {
+    type Error = color_eyre::Report;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
+        }
+    }
+}
+
+impl Move {
+    /// How many points do we get for picking that move?
+    fn inherent_points(self) -> usize {
+        match self {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3,
+        }
+    }
+}
+
+impl Move {
+    fn beats(self, other: Move) -> bool {
+        matches!(
+            (self, other),
+            (Self::Rock, Self::Scissors)
+                | (Self::Paper, Self::Rock)
+                | (Self::Scissors, Self::Paper)
+        )
+    }
+
+    fn outcome(self, theirs: Move) -> Outcome {
+        if self.beats(theirs) {
+            Outcome::Win
+        } else if theirs.beats(self) {
+            Outcome::Loss
+        } else {
+            Outcome::Draw
+        }
+    }
+}
+
+impl FromStr for Round {
+    type Err = color_eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let (Some(theirs), Some(' '), Some(ours), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err(color_eyre::eyre::eyre!("expected <theirs>SP<ours>EOF, got {s:?}"));
+        };
+
+        Ok(Self {
+            theirs: theirs.try_into()?,
+            ours: ours.try_into()?,
+        })
+    }
+}
+
+impl Round {
+    fn outcome(self) -> Outcome {
+        self.ours.outcome(self.theirs)
+    }
+
+    fn our_score(self) -> usize {
+        self.ours.inherent_points() + self.outcome().inherent_points()
+    }
+}
+
+impl Outcome {
+    fn inherent_points(self) -> usize {
+        match self {
+            Outcome::Win => 6,
+            Outcome::Draw => 3,
+            Outcome::Loss => 0,
+        }
+    }
+}
+
+fn main() -> color_eyre::Result<(), Report> {
+    color_eyre::install()?;
+
+    let rounds: Vec<Round> = include_str!("input.txt")
+        .lines()
+        .map(|line| line.parse())
+        .collect::<Result<_, _>>()?;
+    let total_score: usize = rounds.iter().map(|r| r.our_score()).sum();
+    dbg!(total_score);
+
+    Ok(())
+}
