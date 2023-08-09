07c68bf4a781351ccbaa2fd26f4a95aa926cbb9c
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2023-08-09 19:48:15 +0300

    Solve day 3 part 1

---
+++ day3/src/main.rs
@@ -0,0 +1,69 @@
+mod item {
+    #[repr(transparent)]
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    pub(crate) struct Item(u8);
+
+    impl TryFrom<u8> for Item {
+        type Error = color_eyre::Report;
+
+        fn try_from(value: u8) -> Result<Self, Self::Error> {
+            match value {
+                b'a'..=b'z' | b'A'..=b'Z' => Ok(Item(value)),
+                _ => Err(color_eyre::eyre::eyre!(
+                    "{} is not a valid item",
+                    value as char
+                )),
+            }
+        }
+    }
+
+    impl std::fmt::Debug for Item {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.0 as char)
+        }
+    }
+
+    impl Item {
+        pub(crate) fn score(self) -> usize {
+            match self {
+                Item(b'a'..=b'z') => 1 + (self.0 - b'a') as usize,
+                Item(b'A'..=b'Z') => 27 + (self.0 - b'A') as usize,
+                _ => unreachable!(),
+            }
+        }
+    }
+}
+
+use item::Item;
+
+fn main() -> color_eyre::Result<()> {
+    let mut total_score = 0;
+
+    for line in include_str!("input.txt").lines() {
+        let (first, second) = line.split_at(line.len() / 2);
+
+        let first_items = first
+            .bytes()
+            .map(Item::try_from)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        let dupe_score = second
+            .bytes()
+            .map(Item::try_from)
+            .find_map(|item| {
+                item.ok().and_then(|item| {
+                    first_items
+                        .iter()
+                        .copied()
+                        .find(|&first_item| first_item == item)
+                })
+            })
+            .expect("there should be exactly one duplicate")
+            .score();
+        dbg!(dupe_score);
+        total_score += dupe_score;
+    }
+
+    dbg!(total_score);
+    Ok(())
+}
