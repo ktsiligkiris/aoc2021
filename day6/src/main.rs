6a3923b3fb86ad5ebc774e0d2fd092a371cce22f
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2023-08-31 20:20:07 +0300

    Solve day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,11 +1,29 @@
-use std::collections::HashSet;
+trait LowercaseLetter {
+    fn to_u32_for_bitset(&self) -> u32;
+}
+
+impl LowercaseLetter for u8 {
+    fn to_u32_for_bitset(&self) -> u32 {
+        assert!(self.is_ascii_lowercase());
+        1 << (*self as u32 - 'a' as u32)
+    }
+}
 
 fn find_marker(input: &str) -> Option<usize> {
+    const SEQUENCE_SIZE: usize = 14;
+
     input
         .as_bytes()
-        .windows(4)
-        .position(|window| window.iter().collect::<HashSet<_>>().len() == 4)
-        .map(|pos| pos + 4)
+        .windows(SEQUENCE_SIZE)
+        .position(|window| {
+            window
+                .iter()
+                .map(|c| c.to_u32_for_bitset())
+                .fold(0, |acc, x| acc | x)
+                .count_ones() as usize
+                == SEQUENCE_SIZE
+        })
+        .map(|pos| pos + SEQUENCE_SIZE)
 }
 
 fn main() {
