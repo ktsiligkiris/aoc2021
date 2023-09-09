80eeff15b150bca996110a58ce9351de386c49b9
Kostas Tsiligkiris <ktsiligkiris@outlook.com>
2023-09-09 11:05:30 +0300

    Solve day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -5,29 +5,16 @@ mod grid;
 fn main() {
     let grid = parse_grid(include_str!("input.txt"));
 
-    let all_coords = (0..grid.height()).into_iter().flat_map(|y| {
-        (0..grid.width())
-            .into_iter()
-            .map(move |x| GridCoord::from((x, y)))
-    });
+    let all_coords = (0..grid.height())
+        .into_iter()
+        .flat_map(|y| (0..grid.width()).map(move |x| GridCoord::from((x, y))));
 
-    let num_visible_cells = all_coords
-        .filter(|&coord| {
-            let coord_height = grid.cell(coord).unwrap();
-            let deltas: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
-            deltas.iter().any(|&(dx, dy)| {
-                let mut cells_in_ine = (1..).into_iter().map_while(|i| {
-                    let coord = GridCoord {
-                        x: coord.x.checked_add_signed(dx * i)?,
-                        y: coord.y.checked_add_signed(dy * i)?,
-                    };
-                    grid.cell(coord)
-                });
-                cells_in_ine.all(|height| height < coord_height)
-            })
-        })
-        .count();
-    dbg!(num_visible_cells);
+    let best_place = all_coords
+        .map(|coord| (coord, scenic_score(&grid, coord)))
+        .max_by_key(|(_, score)| *score)
+        .unwrap();
+
+    println!("{best_place:?}");
 }
 
 fn parse_grid(input: &str) -> Grid<usize> {
@@ -43,4 +30,31 @@ fn parse_grid(input: &str) -> Grid<usize> {
     }
 
     grid
+}
+
+fn visible_trees_in_dir(grid: &Grid<usize>, coord: GridCoord, (dx, dy): (isize, isize)) -> usize {
+    let line = (1..).into_iter().map_while(|i| {
+        let coord = GridCoord {
+            x: coord.x.checked_add_signed(dx * i)?,
+            y: coord.y.checked_add_signed(dy * i)?,
+        };
+        Some(*grid.cell(coord)?)
+    });
+
+    let mut total = 0;
+    let our_height = *grid.cell(coord).unwrap();
+    for height in line {
+        total += 1;
+        if height >= our_height {
+            break;
+        }
+    }
+    total
+}
+
+fn scenic_score(grid: &Grid<usize>, coord: GridCoord) -> usize {
+    let dirs: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+    dirs.into_iter()
+        .map(|(dx, dy)| visible_trees_in_dir(grid, coord, (dx, dy)))
+        .product()
 }
